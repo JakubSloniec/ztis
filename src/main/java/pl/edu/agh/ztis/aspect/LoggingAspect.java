be4d87e3963c2992cb255d5251503220993a4ae5
JakubSloniec <jakub.sloniec@gmail.com>
2016-03-21 11:21:39 +0100

    exemplary architecture

---
+++ src/main/java/pl/edu/agh/ztis/aspect/LoggingAspect.java
@@ -0,0 +1,84 @@
+package pl.edu.agh.ztis.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * Created by Jakub Sloniec on 10.02.2016.
+ */
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(* pl.edu.agh.ztis.controller..*.*(..))")
+    public void restControlPointcut() {
+    }
+
+    @Pointcut("execution(* pl.edu.agh.ztis.service..*.*(..))")
+    public void sevicePointcut() {
+    }
+
+    @Pointcut("restControlPointcut() || sevicePointcut()")
+    public void combinedPointcut() {
+    }
+
+    @Around("combinedPointcut()")
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        final Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass().getName());
+        Object retVal;
+
+        try {
+            StringBuffer startMessageStringBuffer = new StringBuffer();
+
+            startMessageStringBuffer.append("Start method ");
+            startMessageStringBuffer.append(joinPoint.getSignature().getName());
+            startMessageStringBuffer.append("(");
+
+            Object[] args = joinPoint.getArgs();
+            for (Object arg : args) {
+                startMessageStringBuffer.append(arg).append(",");
+            }
+            if (args.length > 0) {
+                startMessageStringBuffer.deleteCharAt(startMessageStringBuffer.length() - 1);
+            }
+
+            startMessageStringBuffer.append(")");
+
+            logger.info(startMessageStringBuffer.toString());
+
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+
+            retVal = joinPoint.proceed();
+
+            stopWatch.stop();
+
+            StringBuffer endMessageStringBuffer = new StringBuffer();
+            endMessageStringBuffer.append("Finish method ");
+            endMessageStringBuffer.append(joinPoint.getSignature().getName());
+            endMessageStringBuffer.append("(..); execution time: ");
+            endMessageStringBuffer.append(stopWatch.getTotalTimeMillis());
+            endMessageStringBuffer.append(" ms;");
+
+            logger.info(endMessageStringBuffer.toString());
+        } catch (Throwable ex) {
+            StringBuffer errorMessageStringBuffer = new StringBuffer();
+
+            // Create error message
+            logger.error(errorMessageStringBuffer.toString(), ex);
+
+            throw ex;
+        }
+
+        return retVal;
+    }
+}
+
---
+++ src/main/java/pl/edu/agh/ztis/controller/NoteController.java
@@ -0,0 +1,23 @@
+package pl.edu.agh.ztis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.ztis.entity.Note;
+import pl.edu.agh.ztis.service.NoteService;
+
+import java.util.List;
+
+/**
+ * Created by Jakub Sloniec on 21.03.2016.
+ */
+
+@RestController
+public class NoteController {
+
+    @Autowired
+    private NoteService noteService;
+
+    List<Note> getAll() {
+        return noteService.getAll();
+    }
+}
