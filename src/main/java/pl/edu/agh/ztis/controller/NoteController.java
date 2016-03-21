7d1ba3973e93a20c8e74d0a8a721d06cdc6ec099
JakubSloniec <jakub.sloniec@gmail.com>
2016-03-21 11:23:33 +0100

    minor

--- src/main/java/pl/edu/agh/ztis/controller/NoteController.java
+++ src/main/java/pl/edu/agh/ztis/controller/NoteController.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.ztis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.ztis.entity.Note;
 import pl.edu.agh.ztis.service.NoteService;
@@ -17,6 +18,7 @@ public class NoteController {
     @Autowired
     private NoteService noteService;
 
+    @RequestMapping("/all")
     List<Note> getAll() {
         return noteService.getAll();
     }
